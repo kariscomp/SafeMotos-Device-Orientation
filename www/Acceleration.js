c561e1b772aab909fd595fee9f61387f8bf4934a
Peter <kariscomp@gmail.com>
2015-03-03 19:21:18 +0000

    Inital Commit

--- www/Acceleration.js
+++
@@ -1,9 +0,0 @@
-
-var Orientation = function(azimuth, pitch, roll, timestamp) {
-    this.azimuth = azimuth;
-    this.pitch = pitch;
-    this.roll = roll;
-    this.timestamp = timestamp || (new Date()).getTime();
-};
-
-module.exports = Orientation;
